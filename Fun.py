f24f29a0f60e0e40f35e4d9692ca86f202829257
Vialian Harry <96583612+Sycoanonymous@users.noreply.github.com>
2022-05-09 00:22:04 +0500

    Create Fun.py

---
+++ Fun.py
@@ -0,0 +1,17 @@
+logo="""                  ▇◤▔▔▔▔▔▔▔◥▇
+                  ▇▏◥▇◣┊◢▇◤▕▇
+                  ▇▏▃▆▅▎▅▆▃▕▇
+                  ▇▏╱▔▕▎▔▔╲▕▇
+                  ▇◣◣▃▅▎▅▃◢◢▇
+                  ▇▇◣◥▅▅▅◤◢▇▇
+                  ▇▇▇◣╲▇╱◢▇▇▇ 
+//////////////////Haroon khan//////////////////"""
+
+print(logo)
+name=input("What is your name? ")
+print("Wellcome2_hackers_worlds"+name)
+def login():
+os.system("reset")
+try
+token =open("login.txt")
+menu()
